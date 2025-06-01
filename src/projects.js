c24a5dd1d33a459e00144a093b1ca3c9e7398d81
Arcmazter <aronlex@gmail.com>
2025-06-01 17:54:10 +0000

    added Arrested Wateers

--- src/projects.js
+++ src/projects.js
@@ -43,13 +43,13 @@ const projects = [
       "/images/Florist/Flor2.PNG",
       "/images/Florist/Flor3.PNG",
     ],
-    video: "/videos/awesome-demo.mp4",
-    link: "https://yourapp.com",
+    video: "https://youtu.be/gTg6naNyKA4",
+    link: "https://arch-mazter.itch.io/florist",
     sections: [
       {
         title: "Overview",
         content:
-          "Crawling Chaos is a VR game, where you embody Chaos, an unshaped creature set loose to hunt the city of Croix-des-Cendres. The city is governed by the harsh, fire-and-brimstone rhetoric of the church, where outsiders are mistreated and both strangers and monsters are relentlessly hunted. \n\nAs Chaos, you'll need to avoid the Hunters while navigating the city’s rooftops and alleyways to survive.",
+          "The game was developed by a team of three. It tells the story of a man living in the forest with his daughter. Each day, he ventures out in search of a rare flower that helps keep her illness at bay.\n\nBut the forest is shrouded in darkness and teeming with sinister creatures lurking in the shadows, ready to attack. Be cautious and ensure you retrieve the flower in time or it may be too late to save his daughter.",
       },
       {
         title: "The Experience",
@@ -60,6 +60,38 @@ const projects = [
         title: "What I handled",
         content:
           "Being the one with the most experience in game development in the team, I was put in charge as the lead programmer.\n\nAlso, because I had a lot of passion for this game, I took it upon myself to do extra work and did all the art assets myself. The art is rough and stylised but am really happy with it.",
+      },
+    ],
+  },
+  {
+    id: "Water",
+    title: "Arrested Water",
+    shortDescription:
+      "The game was developed by a team of three. It follows the story of a man who becomes lost at sea and wakes up stranded on a hostile beach.\nDetermined to escape, he sets out with his Blunder Axe to gather materials needed to repair a boat and make his way off the island.\n\nThe player can explore various islands, searching for loot and battling enemies, while also encountering NPCs who provide valuable insights and lore about the mysterious place they've become stranded on.",
+    images: [
+      "/images/Water/water-thumb.png",
+      "/images/Water/Water1.PNG",
+      "/images/Water/Water2.PNG",
+      "/images/Water/Water3.PNG",
+      "/images/Water/Water4.PNG",
+    ],
+    video: "/videos/awesome-demo.mp4",
+    link: "https://yourapp.com",
+    sections: [
+      {
+        title: "Overview",
+        content:
+          "The game was developed by a team of three. It follows the story of a man who becomes lost at sea and wakes up stranded on a hostile beach.\nDetermined to escape, he sets out with his Blunder Axe to gather materials needed to repair a boat and make his way off the island.\n\nThe player can explore various islands, searching for loot and battling enemies, while also encountering NPCs who provide valuable insights and lore about the mysterious place they've become stranded on.",
+      },
+      {
+        title: "The Experience",
+        content:
+          "The game was created in just seven days, where the team was able to reuse much of our previous code and assets. After spending a lot of time playing it myself, I always find myself coming back to experience the smooth character movement and combat. I believe it feels really polished and I would definitely love to develop a full-scale version of this game in the future.",
+      },
+      {
+        title: "What I handled",
+        content:
+          "Our team had plenty of talented programmers, which allowed us to include a lot in this game. I handled the player controls, combat, and enemy AI. I also managed the world design and all the sound in the game. \n\nAdditionally, I was in charge of creating all the art and effects; I intentionally kept them rough to allocate more time for programming.",
       },
     ],
   },
